562cb0561a8e4dbb490f6cc0dac28e354192424a
Yann Thomas-Gérard <inside@gmail.com>
2019-01-16 17:47:18 +0100

    initial commit

---
+++ a.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Test  extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          I'm a paragraph
+        </p>
+        <p>
+          I should be red.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Test
